bc761da61fd5a55040a0c9cd69bdb5a873738024
keemsora <itsyoursora@gmail.com>
2022-09-19 21:58:18 +0900

    [백준] 기본 수학2 - 1978_소수 찾기 - 2581_소수 - 11653_소인수분해 - 1929_소수 구하기

---
+++ baekjoon/src/stage/eight_math/Main0802.java
@@ -0,0 +1,51 @@
+package stage.eight_math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main0802 {
+
+    public static boolean prime[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
+        prime = new boolean[N + 1];
+        get_prime();
+
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        for(int i = M; i <= N; i++) {
+            if(prime[i] == false) {
+                sum += i;
+                if(min == Integer.MAX_VALUE) {
+                    min = i;
+                }
+            }
+        }
+
+        if(sum == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
+
+    // 에라토스테네스 체 알고리즘
+    public static void get_prime() {
+        prime[0] = true;
+        prime[1] = true;
+
+        for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+            if(prime[i]) continue;
+            for(int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
+            }
+        }
+    }
+}
