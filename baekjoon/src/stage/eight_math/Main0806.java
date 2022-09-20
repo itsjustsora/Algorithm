a03062128c5298808966ee46d9b82c76240fc8c3
keemsora <itsyoursora@gmail.com>
2022-09-20 22:51:30 +0900

    [백준] 기본 수학 2 - 4948_베르트랑 공준 - 9020_골드바흐의 추측

---
+++ baekjoon/src/stage/eight_math/Main0806.java
@@ -0,0 +1,56 @@
+package stage.eight_math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main0806 {
+
+    /**
+     * 골드바흐의 추측
+     * 2보다 큰 모든 짝수는 두 소수의 합으로 나타낼 수 있다.
+     *
+     * 골드바흐 파티션
+     * 짝수를 두 소수의 합으로 나타내는 표현
+     * 4 = 2 + 2, 6 = 3 + 3, 8 = 3 + 5, ...
+     */
+
+    public static boolean[] prime = new boolean[10001];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        get_prime();
+
+        int N = Integer.parseInt(br.readLine());
+
+        while (N-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int first = n / 2;
+            int second = n / 2;
+
+            while (true) {
+                if(!(prime[first]) && !(prime[second])) {
+                    sb.append(first).append(" ").append(second).append("\n");
+                    break;
+                }
+                first--;
+                second++;
+            }
+        }
+        System.out.println(sb);
+
+    }
+
+    public static void get_prime() {
+        prime[0] = prime[1] = true;
+
+        for (int i = 2; i <= Math.sqrt(prime.length); i++) {
+            if (prime[i]) continue;
+            for (int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
+            }
+        }
+    }
+}
