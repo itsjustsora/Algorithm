c233667edf203a95e63bb0e20a62a73c36705c1a
keemsora <itsyoursora@gmail.com>
2022-09-25 16:40:12 +0900

    [백준] 브루트 포스 - 2798_블랙잭 - 2231_분해합 - 7568_덩치 - 1463_영화감독 숌

---
+++ baekjoon/src/stage/eleven_bruteForce/Main1104.java
@@ -0,0 +1,19 @@
+package stage.eleven_bruteForce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main1104 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        // 나중에 다시 풀어보기
+
+    }
+}
