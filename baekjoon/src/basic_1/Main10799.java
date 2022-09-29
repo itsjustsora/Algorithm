12de084330b8d6f0ee777606087be7c23c78aead
keemsora <itsyoursora@gmail.com>
2022-09-29 21:04:40 +0900

    [백준] 알고리즘 1/2 - 17413_단어 뒤집기 2 - 10799_쇠막대기 - 17298_오큰수

---
+++ baekjoon/src/basic_1/Main10799.java
@@ -0,0 +1,33 @@
+package basic_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main10799 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        Stack<Integer> stack = new Stack<>();
+        int count = 0;
+
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == '(') {
+                stack.push(i);
+                continue;
+            }
+
+            if (str.charAt(i) == ')') {
+                stack.pop();
+                if(str.charAt(i - 1) == '(') {
+                    count += stack.size();
+                } else {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
