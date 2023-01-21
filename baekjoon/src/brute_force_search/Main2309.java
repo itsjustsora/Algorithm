d3fede88679dd6f12b6cdd778c3256c48a7bf038
keemsora <itsyoursora@gmail.com>
2023-01-21 22:12:25 +0900

    [baekjoon] Brute-force Search - 2309_일곱 난쟁이

--- baekjoon/src/brute_force_search/Main2309.java
+++ baekjoon/src/brute_force_search/Main2309.java
@@ -6,32 +6,33 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main2309 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] ppl = new int[9];
+
+        int[] arr = new int[9];
         int sum = 0;
 
-        for (int i = 0; i < ppl.length; i++) {
-          ppl[i] = Integer.parseInt(br.readLine());
-          sum += ppl[i];
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
         }
 
-        for(int i = 0; i < ppl.length - 1; i++) {
-            for (int j = i+1; j < ppl.length; j++) {
-                if (sum - (ppl[i] + ppl[j]) == 100) {
-                    ppl[i] = 0;
-                    ppl[j] = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (sum - (arr[i] + arr[j]) == 100) {
+                    arr[i] = 0;
+                    arr[j] = 0;
 
-                    Arrays.sort(ppl);
+                    Arrays.sort(arr);
 
-                    for(int k = 2; k < ppl.length; k++) {
-                        System.out.println(ppl[k]);
+                    for (int k = 2; k < arr.length; k++) {
+                        System.out.println(arr[k]);
                     }
                     return;
                 }
             }
+
         }
-
-
     }
 }
