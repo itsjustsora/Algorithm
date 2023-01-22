8a05e4a8cf164e9458c032446dce68b25ed260a5
keemsora <itsyoursora@gmail.com>
2023-01-22 22:52:50 +0900

    [baekjoon] Brute-force Search - 2231_분해합

---
+++ baekjoon/src/brute_force_search/Main2231.java
@@ -0,0 +1,28 @@
+package brute_force_search;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main2231 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int answer = 0;
+
+        for (int i = 0; i < N; i++) {
+            int number = i;
+            int sum = 0;
+
+            while (number > 0) {
+                sum += (number % 10);
+                number /= 10;
+            }
+            if (sum + i == N) {
+                answer = i;
+                break;
+            }
+        }
+        System.out.println(answer);
+    }
+}
