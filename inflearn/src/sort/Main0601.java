a87e778b256c7cbf55ba1eade1ada906477d6d85
keemsora <itsyoursora@gmail.com>
2023-02-28 20:16:14 +0900

    [inflearn] sort - 0601_선택 정렬 - 0602_버블 정렬 - 0603_삽입 정렬

---
+++ inflearn/src/sort/Main0601.java
@@ -0,0 +1,32 @@
+package sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main0601 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for (int i = 0; i < N-1; i++) {
+			for (int j = i+1; j < N; j++) {
+				if (arr[i] > arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		for (int i : arr) {
+			System.out.print(i + " ");
+		}
+	}
+}
