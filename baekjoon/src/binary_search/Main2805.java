9790df8a321a3ccec13bccc1c8d652096a3a2013
keemsora <itsyoursora@gmail.com>
2023-06-01 14:59:36 +0900

    [baekjoon] 변경사항 적용

--- baekjoon/src/binary_search/Main2805.java
+++ baekjoon/src/binary_search/Main2805.java
@@ -3,7 +3,6 @@ package binary_search;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main2805 {
