746b0b69173e85bffd42182d7be4003933ffce3b
keemsora <itsyoursora@gmail.com>
2023-06-21 23:03:24 +0900

    [Lv1] 푸드 파이트 대회

---
+++ programmers/src/lv1/푸드_파이트_대회.java
@@ -0,0 +1,19 @@
+package lv1;
+
+public class 푸드_파이트_대회 {
+	public String solution(int[] food) {
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 1; i < food.length; i++) {
+			int num = food[i] / 2;
+			for (int j = 0; j < num; j++) {
+				sb.append(i);
+			}
+		}
+
+		String answer = sb.toString() + "0"
+			+ sb.reverse().toString();
+
+		return answer;
+	}
+}
