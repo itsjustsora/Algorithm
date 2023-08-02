e086c2c116e75ab67c5fdf4a569c9953af1d9504
keemsora <itsyoursora@gmail.com>
2023-08-02 17:49:52 +0900

    [이코테] 구현 - 시각

---
+++ 이코테/src/시각.java
@@ -0,0 +1,34 @@
+package src;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 시각 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int count = 0;
+
+		for (int i = 0; i < N+1; i++) {
+			for (int j = 0; j <60; j++) {
+				for (int k = 0; k < 60; k++) {
+					if (check(i, j, k)) {
+						count++;
+					}
+				}
+			}
+		}
+
+		System.out.println(count);
+	}
+
+	// '3'이 포함되어 있는지 여부 확인
+	public static boolean check(int h, int m, int s) {
+		if (h / 10 == 3 || m / 10 == 3 || s / 10 == 3 || h % 10 == 3 ||
+		m % 10 == 3 || s % 10 == 3) {
+			return true;
+		}
+		return false;
+	}
+}
