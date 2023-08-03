ae0491d4612a0feabeb4244be876a4ff64048a1d
keemsora <itsyoursora@gmail.com>
2023-08-03 15:00:52 +0900

    [이코테] 정렬 - 위에서 아래로

---
+++ 이코테/src/위에서_아래로.java
@@ -0,0 +1,36 @@
+package src;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class 위에서_아래로 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		Integer[] arr = new Integer[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		// for (int i = 1; i < N-1; i++) {
+		// 	for (int j = i; j < N; j++) {
+		// 		if (arr[j] > arr[j - 1]) {
+		// 			int temp = arr[j];
+		// 			arr[j] = arr[j-1];
+		// 			arr[j-1] = temp;
+		// 		}
+		// 	}
+		// }
+
+		Arrays.sort(arr, Collections.reverseOrder());
+
+		for (int i = 0; i < N; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
