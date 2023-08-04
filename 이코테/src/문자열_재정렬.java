43da97e7ffb38d20f6573d77808ba2ad3f20f2f7
keemsora <itsyoursora@gmail.com>
2023-08-04 22:02:05 +0900

    [이코테] 구현 - 문자열 재정렬

---
+++ 이코테/src/문자열_재정렬.java
@@ -0,0 +1,37 @@
+package src;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class 문자열_재정렬 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String line = br.readLine();
+
+		List<String> alphabets = new ArrayList<>();
+		int sum = 0;
+
+		for (int i = 0; i < line.length(); i++) {
+			if ('A' <= line.charAt(i) && line.charAt(i) <= 'Z') { // Character.isLetter()
+				alphabets.add(String.valueOf(line.charAt(i)));
+			} else {
+				sum += line.charAt(i) - '0';
+			}
+		}
+
+
+		Collections.sort(alphabets);
+
+		for (String str : alphabets) {
+			sb.append(str);
+		}
+
+		if (sum != 0) sb.append(sum);
+		System.out.println(sb);
+	}
+}
