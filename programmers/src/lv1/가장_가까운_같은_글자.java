64e37dfdab98583ef404222464952ed74ad2ebd9
keemsora <itsyoursora@gmail.com>
2023-08-11 23:52:50 +0900

    [lv1] update 가장 가까운 같은 글자

--- programmers/src/lv1/가장_가까운_같은_글자.java
+++ programmers/src/lv1/가장_가까운_같은_글자.java
@@ -1,24 +1,17 @@
 package lv1;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class 가장_가까운_같은_글자 {
-	public List solution(String s) {
-		List<Integer> list = new ArrayList<>();
-		list.add(-1);
-
-		for (int i = 1; i < s.length(); i++) {
+	public int[] solution(String s) {
+		int[] answer = new int[s.length()];
+		HashMap<Character,Integer> map = new HashMap<>();
+		for(int i=0; i<s.length();i++){
 			char ch = s.charAt(i);
-			int count = 0;
-			for (int j = 0; j < i; j++) {
-				if (s.charAt(j) == ch) {
-					count = i - j;
-				}
-			}
-			if (count == 0) list.add(-1);
-			else list.add(count);
+			answer[i] = i-map.getOrDefault(ch,i+1);
+			map.put(ch,i);
 		}
-		return list;
+		return answer;
 	}
 }
