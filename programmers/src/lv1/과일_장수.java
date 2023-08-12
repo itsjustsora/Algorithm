ac2909c0b86389a2454ce35e731f348f7f0bb583
keemsora <itsyoursora@gmail.com>
2023-08-12 15:59:04 +0900

    [Lv1] update 과일 장수

--- programmers/src/lv1/과일_장수.java
+++ programmers/src/lv1/과일_장수.java
@@ -6,13 +6,11 @@ import java.util.Collections;
 public class 과일_장수 {
 	public int solution(int k, int m, int[] score) {
 		int answer = 0;
-		Integer[] arr = Arrays.stream(score).boxed().toArray(Integer[]::new);
-		Arrays.sort(arr, Collections.reverseOrder());
 
-		for (int i = 0; i < score.length; i++) {
-			if ((i+1) % m == 0) {
-				answer += arr[i] * m;
-			}
+		Arrays.sort(score);
+
+		for(int i = score.length; i >= m; i -= m){
+			answer += score[i - m] * m;
 		}
 
 		return answer;
