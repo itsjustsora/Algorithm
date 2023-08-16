7dae4fda2ca0e07ae9f617b26fc6f4bbe35a47e1
keemsora <itsyoursora@gmail.com>
2023-08-17 00:42:32 +0900

    [이코테] 개미 전사

---
+++ 이코테/src/개미_전사.java
@@ -0,0 +1,19 @@
+package src;
+
+public class 개미_전사 {
+
+	public static int[] dp;
+	public static void main(String[] args) {
+		System.out.println(solution(4, new int[] {1, 3, 1, 5}));
+	}
+
+	public static int solution(int n, int[] storage) {
+		dp = new int[n];
+		dp[0] = storage[0];
+		dp[1] = Math.max(storage[0], storage[1]);
+		for (int i = 2; i < n; i++) {
+			dp[i] = Math.max(dp[i - 1], dp[i - 2] + storage[i]);
+		}
+		return dp[n - 1];
+	}
+}
