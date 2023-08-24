473efea29d6c01311c1e835b0d77f5c42ca37602
keemsora <itsyoursora@gmail.com>
2023-08-24 14:56:00 +0900

    [SQL kit] 상품별 오프라인 매출 구하기

---
+++ programmers/src/sqlkit/상품별_오프라인_매출_구하기.sql
@@ -0,0 +1,7 @@
+-- JOIN, SUM, GROUP BY
+SELECT p.product_code, SUM(p.price * o.sales_amount) AS sales
+FROM product AS p
+    JOIN offline_sale AS o
+        ON p.product_id = o.product_id
+GROUP BY p.product_code
+ORDER BY sales DESC, p.product_code;
